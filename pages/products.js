dba5f66a5c425ea35a34a0454c911f93bec5f515
Shawn Kebel <fxrender@outlook.com>
2023-05-14 14:10:54 -0500

    :boom::sparkles: Create `single-prod-page` with `image selector`

--- pages/products.js
+++ pages/products.js
@@ -1,14 +1,14 @@
 import Center from "@/components/Center";
 import Header from "@/components/Header";
 import ProductsGrid from "@/components/ProductsGrid";
+import Title from "@/components/Title";
 import { mongooseConnect } from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 import styled from "styled-components";
 
 export default function ProductsPage({products}) {
 
-const Title = styled.h1`
-  font-size: 1.5em;`;
+
 
   return (
     <>
