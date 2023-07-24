a3851b83501f0d1a053b8cd1dd16a702de63cdaf
Shawn Kebel <fxrender@outlook.com>
2023-07-23 19:23:27 -0500

    :necktie: Finish `index` page

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,40 @@ import Featured from '@/components/Featured';
 import Header from "@/components/Header";
 import { Product } from "@/models/Product";
 import NewProducts from "@/components/NewProducts";
+import {WishedProduct} from "@/models/WishedProduct";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]";
+import { Setting } from "@/models/Setting";
 
-export default function HomePage({ featuredProduct, newProducts }) {
+export default function HomePage({ featuredProduct, newProducts, wishedNewProducts }) {
   return (
     <div>
       <Header />
       <Featured product={featuredProduct} />
-      <NewProducts products={newProducts} />
+      <NewProducts products={newProducts} wishedProducts={wishedNewProducts} />
     </div>
   );
 }
 
-export async function getServerSideProps() {
-  const featuredProductId = "645916e6a4916c1b2d1e259a";
+export async function getServerSideProps(ctx) {
+  const featuredProductSetting = await Setting.findOne({name:'featuredProductId'});
+  const featuredProductId = featuredProductSetting?.value ;
   await mongooseConnect();
   const featuredProduct = await Product.findById(featuredProductId);
   const newProducts = await Product.find({}, null, { sort: { _id: -1 }, limit: 10 });
+  const session = await getServerSession(ctx.req, ctx.res, authOptions);
+
+  const wishedNewProducts = session?.user ? await WishedProduct?.find({
+    userEmail: session.user.email,
+    product: newProducts.map(p => p._id.toString()),
+  }) : [];
+
+
   return {
     props: {
       featuredProduct: JSON.parse(JSON.stringify(featuredProduct)),
       newProducts: JSON.parse(JSON.stringify(newProducts)),
+      wishedNewProducts: wishedNewProducts.map(i => i.product.toString()),
     },
   };
 }
